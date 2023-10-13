3e720bd945091582a0c9f7c57291025415cf5074
ibnaaini <145310307+ibnaaini@users.noreply.github.com>
2023-10-13 09:57:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -70,3 +70,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+// slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Ganti gambar setiap 2 detik
+}
